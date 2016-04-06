41fa7c385053a6372510bb60aaa6b22da0cac842
John McAninley <johnbert314@yahoo.com>
2016-04-06 22:46:11 +0000

    counting articles added to queue

--- src-python/biases/corpus/create_corpus.py
+++ src-python/biases/corpus/create_corpus.py
@@ -1,30 +1,36 @@
+import sys
 from collections import deque
 import argparse
 import mwclient
+from human_relevance import human_relevance
 
 def create_corpus(seed_article, lang_code):
 	
 	articles_to_check = deque()
 	corpus = []
 	seen = []
+
+	relevant = human_relevance
 	
 	site = mwclient.Site(lang_code + '.wikipedia.org')
 	
 	page = site.Pages[seed_article]
-	
+
 	articles_to_check.append(page)
 	
 	while articles_to_check:
 		article = articles_to_check.pop()
 		if relevant(article):
-		links = article.links()
-		for link in links:
-			if link not in seen:
-				articles_to_check.append(article)
-				seen.add(link)
-		corpus.add(article.name)
+			links = article.links()
+			for link in links:
+				if link not in seen:
+					articles_to_check.append(article)
+					seen.append(link)
+					print link.name
+			corpus.append(article.name)
 		
-	print(corpus)
+	for article in corpus:
+		print article.name
 
 if __name__ == '__main__':
 	create_corpus(sys.argv[1], sys.argv[2])
