ca2b4259b1f29b497903619b8703692dd6f205ec
John McAninley <johnbert314@yahoo.com>
2016-04-26 22:29:02 +0000

    Updated corous creation method

--- src-python/biases/corpus/create_corpus.py
+++ src-python/biases/corpus/create_corpus.py
@@ -1,3 +1,4 @@
+import codecs
 import sys
 from collections import deque
 import argparse
@@ -5,6 +6,9 @@ import mwclient
 from human_relevance import human_relevance
 
 def create_corpus(seed_article, lang_code):
+
+	UTF8Writer = codecs.getwriter('utf8')
+	sys.stdout = UTF8Writer(sys.stdout)
 	
 	articles_to_check = deque()
 	corpus = []
@@ -17,20 +21,25 @@ def create_corpus(seed_article, lang_code):
 	page = site.Pages[seed_article]
 
 	articles_to_check.append(page)
+	seen.append(page)
+
+	evaluated = 0
 	
-	while articles_to_check:
+	while articles_to_check and evaluated < 100:
 		article = articles_to_check.pop()
+		print 'ARTICLE: ' + article.name 		
 		if relevant(article):
 			links = article.links()
 			for link in links:
+				print 'LINK: ' + link.name
 				if link not in seen:
-					articles_to_check.append(article)
+					articles_to_check.appendleft(link)
 					seen.append(link)
-					print link.name
 			corpus.append(article.name)
+		evaluated += 1
 		
 	for article in corpus:
-		print article.name
+		print article
 
 if __name__ == '__main__':
 	create_corpus(sys.argv[1], sys.argv[2])
