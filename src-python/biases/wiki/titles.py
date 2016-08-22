90fd4c075b13a7343ad68db1242e9508d6188ddd
cassidylaidlaw <cassidy256@gmail.com>
2016-08-22 10:08:41 -0400

    Fixed typo

--- src-python/biases/wiki/titles.py
+++ src-python/biases/wiki/titles.py
@@ -2,7 +2,7 @@
 
 def make_wiki_title(title):
     """Given a title, returns the official title of the article that it refers
-    to by captilizing the first letter and replacing spaces with
+    to by capitalizing the first letter and replacing spaces with
     underscores. For instance, 'money laundering' becomes 
     'Money_laundering'."""
     
