9315bdd8e7b1ccab23ef0314fb48515b47a41221
cassidylaidlaw <cassidy256@gmail.com>
2017-01-21 14:42:00 -0500

    Added logging to mturk_grant_qualification.py

--- scripts/mturk_grant_qualification.py
+++ scripts/mturk_grant_qualification.py
@@ -4,73 +4,96 @@ import sys
 import json
 import time
 import logging
+import csv
 
 from biases.utils.mturk import mturk_connection
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python3 mturk_grant_qualification.py qualificiation-test.json qual_id')
+    if len(sys.argv) != 4:
+        print('Usage: python3 mturk_grant_qualification.py qualificiation-test.json qual_id log.csv')
         print('Continually checks for submitted qualification tests for the qualification')
         print('with the given ID and grants or rejects them based on the answers in')
-        print('qualification-test.json.')
+        print('qualification-test.json. Writes a log of the results of log.csv.')
     else:
-        _, qual_test_fname, qual_id = sys.argv
+        _, qual_test_fname, qual_id, log_fname = sys.argv
         
         with open(qual_test_fname, 'r') as qual_test_file:
             qual_test_json = json.load(qual_test_file)
             
-        while True:
-            requests = mturk_connection.get_qualification_requests(qual_id)
-            for request in requests:
-                answers = {answer.qid: answer.fields[0] for answer in
-                           request.answers[0]}
-                grant_qualification = True
-                
-                tags_total, tags_correct = 0, 0
-                for selection_index, selection in \
-                        enumerate(qual_test_json['selections']):
+        with open(log_fname, 'w') as log_file:
+            log_csv = csv.writer(log_file)
+            log_headers = ['worker', 'time', 'granted', 'tags-correct']
+            for selection_index, selection in \
+                    enumerate(qual_test_json['selections']):
+                for sentence_index in range(len(selection['correct_tags'])):
+                    log_headers.append('selection-{}-sentence-{}'.format(
+                            selection_index, sentence_index))
+                for endpoint in selection['correct_bias'].keys():
+                    log_headers.append('selection-{}-bias-{}'.format(
+                            selection_index, endpoint))
+            log_csv.writerow(log_headers)
+            
+            while True:
+                requests = mturk_connection.get_qualification_requests(qual_id)
+                for request in requests:
+                    answers = {answer.qid: answer.fields[0] for answer in
+                               request.answers[0]}
+                    grant_qualification = True
                     
-                    # Check bias tags
-                    for sentence_index, tag in \
-                            enumerate(selection['correct_tags']):
-                        qid = 'selection-{}-sentence-{}'.format(
-                                selection_index, sentence_index)
-                        if answers[qid] == tag:
-                            tags_correct += 1
-                        tags_total += 1
+                    tags_total, tags_correct = 0, 0
+                    for selection_index, selection in \
+                            enumerate(qual_test_json['selections']):
                         
-                    # Check bias scores
-                    for endpoint, correct_bias in \
-                            selection['correct_bias'].items():
-                        qid = 'selection-{}-bias-{}'.format(selection_index,
-                                                            endpoint)
-                        # Reject qualification if bias score is off by more
-                        # than 1
-                        answer_bias = int(answers[qid])
-                        if abs(answer_bias - correct_bias) > 1:
-                            grant_qualification = False
-                        logging.info('%s on %s: answer=%d, correct=%d',
-                                request.SubjectId, qid, answer_bias,
-                                correct_bias)
-                 
-                logging.info('%s: %d / %d tags correct (%d%%)',
-                        request.SubjectId, tags_correct, tags_total,
-                        round(tags_correct / tags_total * 100))
-                # Reject qualification if less than 80% of tags are correct           
-                if tags_correct / tags_total < 0.8:
-                    grant_qualification = False
-                    
-                if grant_qualification:
-                    mturk_connection.grant_qualification(
-                            request.QualificationRequestId)
-                    logging.info('%s: granted qualification', 
-                            request.SubjectId)
-                else:
-                    mturk_connection._process_request(
-                            'RejectQualificationRequest',
-                            {'QualificationRequestId':
-                             request.QualificationRequestId})
-                    logging.info('%s: rejected qualification',
-                            request.SubjectId)
-                    
-            time.sleep(10)
+                        # Check bias tags
+                        for sentence_index, tag in \
+                                enumerate(selection['correct_tags']):
+                            qid = 'selection-{}-sentence-{}'.format(
+                                    selection_index, sentence_index)
+                            if answers[qid] == tag:
+                                tags_correct += 1
+                            tags_total += 1
+                            
+                        # Check bias scores
+                        for endpoint, correct_bias in \
+                                selection['correct_bias'].items():
+                            qid = 'selection-{}-bias-{}'.format(selection_index,
+                                                                endpoint)
+                            # Reject qualification if bias score is off by more
+                            # than 1
+                            answer_bias = int(answers[qid])
+                            if abs(answer_bias - correct_bias) > 1:
+                                grant_qualification = False
+                            logging.info('%s on %s: answer=%d, correct=%d',
+                                    request.SubjectId, qid, answer_bias,
+                                    correct_bias)
+                     
+                    logging.info('%s: %d / %d tags correct (%d%%)',
+                            request.SubjectId, tags_correct, tags_total,
+                            round(tags_correct / tags_total * 100))
+                    # Reject qualification if less than 80% of tags are correct           
+                    if tags_correct / tags_total < 0.8:
+                        grant_qualification = False
+                        
+                    if grant_qualification:
+                        mturk_connection.grant_qualification(
+                                request.QualificationRequestId)
+                        logging.info('%s: granted qualification', 
+                                request.SubjectId)
+                    else:
+                        mturk_connection._process_request(
+                                'RejectQualificationRequest',
+                                {'QualificationRequestId':
+                                 request.QualificationRequestId})
+                        logging.info('%s: rejected qualification',
+                                request.SubjectId)
+                        
+                    log_info = dict(answers) # Make a copy of answers to log
+                    log_info['worker'] = request.SubjectId
+                    log_info['time'] = request.SubmitTime
+                    log_info['granted'] = grant_qualification
+                    log_info['tags-correct'] = tags_correct
+                    log_csv.writerow([log_info[header] for header in
+                                      log_headers])
+                    log_file.flush()
+                        
+                time.sleep(10)
