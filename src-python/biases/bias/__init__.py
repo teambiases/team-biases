eba836d1db13167dc7d4847339ec84286f1dbcc6
cassidylaidlaw <cassidy256@gmail.com>
2017-03-01 16:41:06 -0500

    Added BiasModel class/interface

--- src-python/biases/bias/__init__.py
+++ src-python/biases/bias/__init__.py
@@ -1,1 +1,16 @@
 """Methods for automatic detection of bias."""
+
+class BiasModel(object):
+    """
+    A model for predicting the bias of an article along a particular spectrum.
+    """
+    
+    def predict(self, document, lang):
+        """
+        This method should predict the bias score of a particular document in
+        a particular language. The bias score should range from 0 to 1 (the
+        meaning depends on what endpoints the bias model was trained on). The
+        input document is represented as a list of sentences, where each
+        sentence is a list of tokens.
+        """
+        raise NotImplementedError()
