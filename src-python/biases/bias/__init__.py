5deeef39194b675205b5b823923bacd9217b7173
cassidylaidlaw <cassidy256@gmail.com>
2017-03-08 22:08:12 -0500

    Added RandomBiasModel for testing

--- src-python/biases/bias/__init__.py
+++ src-python/biases/bias/__init__.py
@@ -1,5 +1,7 @@
 """Methods for automatic detection of bias."""
 
+import random
+
 class BiasModel(object):
     """
     A model for predicting the bias of an article along a particular spectrum.
@@ -21,3 +23,24 @@ class BiasModel(object):
         sentence is a list of tokens.
         """
         raise NotImplementedError()
+
+class RandomBiasModel(object):
+    """
+    A model that returns a random bias score for any document. This can be
+    useful for testing code that requires a bias model.
+    """
+    
+    def __init__(self):
+        self.is_loaded = False
+    
+    def load(self):
+        self.is_loaded = True
+        
+    def predict(self, document, lang):
+        if self.is_loaded:
+            # Make sure document is as expected
+            ' '.join(' '.join(sentence) for sentence in document)
+            return random.random()
+        else:
+            raise RuntimeError('The load() method was not called on this '
+                               'model.')
