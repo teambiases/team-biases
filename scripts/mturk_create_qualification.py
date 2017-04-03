fd9a68f34daac30224dabfd2816249bbdd397a62
cassidylaidlaw <cassidy256@gmail.com>
2017-04-03 12:20:35 -0400

    Added mturk qualification tests with just a password field (for manually recruited annotators)

--- scripts/mturk_create_qualification.py
+++ scripts/mturk_create_qualification.py
@@ -7,105 +7,121 @@ from biases.utils.mturk import mturk_connection
 from boto.mturk.question import *
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python3 mturk_create_qualification.py qualificiation-test.json qual_name')
+    if len(sys.argv) not in range(2, 4):
+        print('Usage: python3 mturk_create_qualification.py qual_name qualification-test.json|password')
+        print('If no qualification test file is specified, then creates a qualification test')
+        print('that just asks for the specified password.')
     else:
-        _, qual_test_fname, qual_name = sys.argv
-        
-        with open(qual_test_fname, 'r') as qual_test_file:
-            qual_test_json = json.load(qual_test_file)
+        qual_name = sys.argv[1]
+        if sys.argv[2].endswith('.json'):
+            qual_test_fname = sys.argv[2]
+            qual_password = None
+        else:
+            qual_password = sys.argv[2]
+            qual_test_fname = None
             
         qual_test = QuestionForm()
         
-        overview = Overview()
-        overview.append(SimpleField('Title', 'Resumen de la prueba de calificación'))
-        overview.append(SimpleField('Text',
-                '''Hola, Muchas gracias por dedicar tu tiempo y participar en \
-                esta encuesta. La encuesta busca detectar y entender la \
-                preferencia o inclinacion hacia uno de dos paises (los Estados Unidos 
-				o la Union Sovietica) en una frase o oración en español.\
-                Para el propósito de esta encuesta, esta 'inclinacion' tiene la \
-                definición de existencia de  imbalance, lo que sugiere \
-				la existencia de preferencia o preconcepción. Se \
-                requiere que usted remarque la preferencia en dos frases cortas. \
-                Para poder aprobar en esta calificación, usted deberá completar \
-                la siguiente prueba con una exactitud de 80 %.'''))
-        qual_test.append(overview)
-        
-        for selection_index, selection in \
-                enumerate(qual_test_json['selections']):
-            selection_overview = Overview()
-            selection_overview.append(SimpleField('Title', 'Selección de texto {}'
-                    .format(selection_index + 1)))
-            selection_overview.append(SimpleField('Text',
-                    '''Por favor lea el siguiente texto :'''))
-            selection_overview.append(SimpleField('Text',
-                    ' '.join(selection['sentences'])))
-            selection_overview.append(SimpleField('Text',
-                    '''Para cada oracion, decidir si la inclinacion o preferencia \
-                    existe hacia los Estados Unidos, hacia la Unión Soviética, o si no \
-				    existe ninguna inclinacion.\ 
-				    Cabe resaltar que muchas de las frases no demuestran parcialidad.'''))
-            qual_test.append(selection_overview)
+        if qual_test_fname is not None:
+            with open(qual_test_fname, 'r') as qual_test_file:
+                qual_test_json = json.load(qual_test_file)
             
-            for sentence_index, sentence in enumerate(selection['sentences']):
-                question_identifier = 'selection-{}-sentence-{}' \
-                        .format(selection_index, sentence_index)
-                question_content = QuestionContent()
-                question_content.append(SimpleField('Text', sentence))
-                answer_spec = AnswerSpecification(SelectionAnswer(
-                        style = 'radiobutton',
-                        selections = [
-                            ('No existe preferencia', 'none'),
-                            ('Preferencia hacia los Estados Unidos', 'us-towards'),
-                            ('Preferencia contra los Estados Unidoss', 'us-against'),
-                            ('Preferencia hacia la Unión Soviética', 'soviet-towards'),
-                            ('Preferencia contra la Unión Soviética', 'soviet-against'),
-                        ]))
-                        
-                sentence_question = Question(question_identifier,
-                                             question_content,
+            overview = Overview()
+            overview.append(SimpleField('Title', 'Resumen de la prueba de calificación'))
+            overview.append(SimpleField('Text',
+                    '''Hola, Muchas gracias por dedicar tu tiempo y participar en \
+                    esta encuesta. La encuesta busca detectar y entender la \
+                    preferencia o inclinacion hacia uno de dos paises (los Estados Unidos 
+    				o la Union Sovietica) en una frase o oración en español.\
+                    Para el propósito de esta encuesta, esta 'inclinacion' tiene la \
+                    definición de existencia de  imbalance, lo que sugiere \
+    				la existencia de preferencia o preconcepción. Se \
+                    requiere que usted remarque la preferencia en dos frases cortas. \
+                    Para poder aprobar en esta calificación, usted deberá completar \
+                    la siguiente prueba con una exactitud de 80 %.'''))
+            qual_test.append(overview)
+            
+            for selection_index, selection in \
+                    enumerate(qual_test_json['selections']):
+                selection_overview = Overview()
+                selection_overview.append(SimpleField('Title', 'Selección de texto {}'
+                        .format(selection_index + 1)))
+                selection_overview.append(SimpleField('Text',
+                        '''Por favor lea el siguiente texto :'''))
+                selection_overview.append(SimpleField('Text',
+                        ' '.join(selection['sentences'])))
+                selection_overview.append(SimpleField('Text',
+                        '''Para cada oracion, decidir si la inclinacion o preferencia \
+                        existe hacia los Estados Unidos, hacia la Unión Soviética, o si no \
+    				    existe ninguna inclinacion.\ 
+    				    Cabe resaltar que muchas de las frases no demuestran parcialidad.'''))
+                qual_test.append(selection_overview)
+                
+                for sentence_index, sentence in enumerate(selection['sentences']):
+                    question_identifier = 'selection-{}-sentence-{}' \
+                            .format(selection_index, sentence_index)
+                    question_content = QuestionContent()
+                    question_content.append(SimpleField('Text', sentence))
+                    answer_spec = AnswerSpecification(SelectionAnswer(
+                            style = 'radiobutton',
+                            selections = [
+                                ('No existe preferencia', 'none'),
+                                ('Preferencia hacia los Estados Unidos', 'us-towards'),
+                                ('Preferencia contra los Estados Unidoss', 'us-against'),
+                                ('Preferencia hacia la Unión Soviética', 'soviet-towards'),
+                                ('Preferencia contra la Unión Soviética', 'soviet-against'),
+                            ]))
+                            
+                    sentence_question = Question(question_identifier,
+                                                 question_content,
+                                                 answer_spec)
+                    qual_test.append(sentence_question)
+                
+                bias_overview = Overview()
+                bias_overview.append(SimpleField('Text',
+                        '''Contestar las dos preguntas siguientes acerca de la \
+                        inclinacion en general en el texto. (Todas las frases)'''))
+                qual_test.append(bias_overview)
+                
+                for country_id, country_name in [('us', 'los Estados Unidos'),
+                                                 ('soviet', 'la Unión Soviética')]:
+                    question_identifier = 'selection-{}-bias-{}' \
+                            .format(selection_index, country_id)
+                    question_content = QuestionContent()
+                    question_content.append(SimpleField('Text',
+                            'Cual es el sentido de la inclinacion en el texto? '
+                            + 'Favorable o desfavorable hacia {}?'
+                            .format(country_name)))
+                    answer_spec = AnswerSpecification(SelectionAnswer(
+                            style = 'radiobutton',
+                            selections = [
+                                ('Totalmente en contra de ' + country_name, '-3'),
+                                ('Moderadamente en contra de ' + country_name,
+                                 '-2'),
+                                ('Ligeramente en contra de ' + country_name, '-1'),
+                                ('No existe inclinacion', '0'),
+                                ('Ligeramente parcial hacia ' + country_name, '1'),
+                                ('Moderadamente parcial a favor de ' + country_name,
+                                 '2'),
+                                ('Totalmente parcial a favorde ' + country_name,
+                                 '3')
+                            ]))
+                            
+                    bias_question = Question(question_identifier, question_content,
                                              answer_spec)
-                qual_test.append(sentence_question)
-            
-            bias_overview = Overview()
-            bias_overview.append(SimpleField('Text',
-                    '''Contestar las dos preguntas siguientes acerca de la \
-                    inclinacion en general en el texto. (Todas las frases)'''))
-            qual_test.append(bias_overview)
-            
-            for country_id, country_name in [('us', 'los Estados Unidos'),
-                                             ('soviet', 'la Unión Soviética')]:
-                question_identifier = 'selection-{}-bias-{}' \
-                        .format(selection_index, country_id)
-                question_content = QuestionContent()
-                question_content.append(SimpleField('Text',
-                        'Cual es el sentido de la inclinacion en el texto? '
-                        + 'Favorable o desfavorable hacia {}?'
-                        .format(country_name)))
-                answer_spec = AnswerSpecification(SelectionAnswer(
-                        style = 'radiobutton',
-                        selections = [
-                            ('Totalmente en contra de ' + country_name, '-3'),
-                            ('Moderadamente en contra de ' + country_name,
-                             '-2'),
-                            ('Ligeramente en contra de ' + country_name, '-1'),
-                            ('No existe inclinacion', '0'),
-                            ('Ligeramente parcial hacia ' + country_name, '1'),
-                            ('Moderadamente parcial a favor de ' + country_name,
-                             '2'),
-                            ('Totalmente parcial a favorde ' + country_name,
-                             '3')
-                        ]))
-                        
-                bias_question = Question(question_identifier, question_content,
-                                         answer_spec)
-                qual_test.append(bias_question)
+                    qual_test.append(bias_question)
+        else:
+            question_content = QuestionContent()
+            question_content.append(SimpleField('Text',
+                                                'What is the password?'))
+            password_question = Question('password', question_content,
+                    AnswerSpecification(FreeTextAnswer()))
+            qual_test.append(password_question)
         
-        mturk_connection.create_qualification_type(qual_name,
+        print(mturk_connection.create_qualification_type(qual_name,
                 '''Capaz de leer y entender tendencia en espanol.''',
                 status = 'Active',
                 keywords = ['Spanish', 'bias', 'Espa\xf1ol', 'tag'],
                 test = qual_test,
-                test_duration = 60 * 60) # an hour
+                test_duration = 60 * 60)) # an hour
         
--- scripts/mturk_grant_qualification.py
+++ scripts/mturk_grant_qualification.py
@@ -11,31 +11,41 @@ from biases.utils.mturk import mturk_connection
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print('Usage: python3 mturk_grant_qualification.py qualificiation-test.json qual_id log.csv')
+        print('Usage: python3 mturk_grant_qualification.py qualificiation-test.json|password qual_id log.csv')
         print('Continually checks for submitted qualification tests for the qualification')
         print('with the given ID and grants or rejects them based on the answers in')
-        print('qualification-test.json. Writes a log of the results of log.csv.')
+        print('qualification-test.json. Writes a log of the results of log.csv. If')
+        print('password is specified instead of a qualification test file, then just checks')
+        print('the password field of the qualification test.')
     else:
         _, qual_test_fname, qual_id, log_fname = sys.argv
         
-        with open(qual_test_fname, 'r') as qual_test_file:
-            qual_test_json = json.load(qual_test_file)
+        if not qual_test_fname.endswith('.json'):
+            qual_password = qual_test_fname
+            qual_test_fname = None
+        
+        if qual_test_fname is not None:
+            with open(qual_test_fname, 'r') as qual_test_file:
+                qual_test_json = json.load(qual_test_file)
+        else:
+            qual_test = None
             
         log_already_exists = os.path.isfile(log_fname)
         with open(log_fname, 'a' if log_already_exists else 'w') as log_file:
             log_csv = csv.writer(log_file)
-            log_headers = ['worker', 'request-id', 'time', 'granted', 
-                           'tags-correct']
-            for selection_index, selection in \
-                    enumerate(qual_test_json['selections']):
-                for sentence_index in range(len(selection['correct_tags'])):
-                    log_headers.append('selection-{}-sentence-{}'.format(
-                            selection_index, sentence_index))
-                for endpoint in selection['correct_bias'].keys():
-                    log_headers.append('selection-{}-bias-{}'.format(
-                            selection_index, endpoint))
-            if not log_already_exists:
-                log_csv.writerow(log_headers)
+            if qual_test is not None:
+                log_headers = ['worker', 'request-id', 'time', 'granted', 
+                               'tags-correct']
+                for selection_index, selection in \
+                        enumerate(qual_test_json['selections']):
+                    for sentence_index in range(len(selection['correct_tags'])):
+                        log_headers.append('selection-{}-sentence-{}'.format(
+                                selection_index, sentence_index))
+                    for endpoint in selection['correct_bias'].keys():
+                        log_headers.append('selection-{}-bias-{}'.format(
+                                selection_index, endpoint))
+                if not log_already_exists:
+                    log_csv.writerow(log_headers)
             
             while True:
                 requests = mturk_connection.get_qualification_requests(qual_id)
@@ -43,45 +53,62 @@ if __name__ == '__main__':
                     answers = {answer.qid: (answer.fields[0] if
                                len(answer.fields) > 0 else None)
                                for answer in request.answers[0]}
-                    grant_qualification = True
-                    
-                    tags_total, tags_correct = 0, 0
-                    for selection_index, selection in \
-                            enumerate(qual_test_json['selections']):
+                    if qual_test is not None:
+                        grant_qualification = True
                         
-                        # Check bias tags
-                        for sentence_index, tag in \
-                                enumerate(selection['correct_tags']):
-                            qid = 'selection-{}-sentence-{}'.format(
-                                    selection_index, sentence_index)
-                            if answers[qid] == tag:
-                                tags_correct += 1
-                            tags_total += 1
+                        tags_total, tags_correct = 0, 0
+                        for selection_index, selection in \
+                                enumerate(qual_test_json['selections']):
                             
-                        # Check bias scores
-                        for endpoint, correct_bias in \
-                                selection['correct_bias'].items():
-                            qid = 'selection-{}-bias-{}'.format(selection_index,
-                                                                endpoint)
-                            # Reject qualification if bias score is off by more
-                            # than 1
-                            try:
-                                answer_bias = int(answers[qid])
-                                if abs(answer_bias - correct_bias) > 1:
+                            # Check bias tags
+                            for sentence_index, tag in \
+                                    enumerate(selection['correct_tags']):
+                                qid = 'selection-{}-sentence-{}'.format(
+                                        selection_index, sentence_index)
+                                if answers[qid] == tag:
+                                    tags_correct += 1
+                                tags_total += 1
+                                
+                            # Check bias scores
+                            for endpoint, correct_bias in \
+                                    selection['correct_bias'].items():
+                                qid = 'selection-{}-bias-{}'.format(
+                                        selection_index, endpoint)
+                                # Reject qualification if bias score is off by
+                                # more than 1
+                                try:
+                                    answer_bias = int(answers[qid])
+                                    if abs(answer_bias - correct_bias) > 1:
+                                        grant_qualification = False
+                                # If they didn't give an answer
+                                except TypeError: 
                                     grant_qualification = False
-                            except TypeError: # If they didn't give an answer
-                                grant_qualification = False
-                            logging.info('%s on %s: answer=%s, correct=%d',
-                                    request.SubjectId, qid, answers[qid],
-                                    correct_bias)
-                     
-                    logging.info('%s: %d / %d tags correct (%d%%)',
-                            request.SubjectId, tags_correct, tags_total,
-                            round(tags_correct / tags_total * 100))
-                    # Reject qualification if less than 80% of tags are correct           
-                    if tags_correct / tags_total < 0.8:
-                        grant_qualification = False
-                        
+                                logging.info('%s on %s: answer=%s, correct=%d',
+                                        request.SubjectId, qid, answers[qid],
+                                        correct_bias)
+                         
+                        logging.info('%s: %d / %d tags correct (%d%%)',
+                                request.SubjectId, tags_correct, tags_total,
+                                round(tags_correct / tags_total * 100))
+                        # Reject qualification if less than 80% of tags are
+                        # correct           
+                        if tags_correct / tags_total < 0.8:
+                            grant_qualification = False
+                            
+                        # Make a copy of answers to log
+                        log_info = dict(answers)
+                        log_info['worker'] = request.SubjectId
+                        log_info['request-id'] = request.QualificationRequestId
+                        log_info['time'] = request.SubmitTime
+                        log_info['granted'] = grant_qualification
+                        log_info['tags-correct'] = tags_correct
+                        log_csv.writerow([log_info[header] for header in
+                                          log_headers])
+                        log_file.flush()
+                    else:
+                        grant_qualification = answers['password'] == \
+                                qual_password
+                            
                     if grant_qualification:
                         mturk_connection.grant_qualification(
                                 request.QualificationRequestId)
@@ -94,16 +121,6 @@ if __name__ == '__main__':
                                  request.QualificationRequestId})
                         logging.info('%s: rejected qualification',
                                 request.SubjectId)
-                        
-                    log_info = dict(answers) # Make a copy of answers to log
-                    log_info['worker'] = request.SubjectId
-                    log_info['request-id'] = request.QualificationRequestId
-                    log_info['time'] = request.SubmitTime
-                    log_info['granted'] = grant_qualification
-                    log_info['tags-correct'] = tags_correct
-                    log_csv.writerow([log_info[header] for header in
-                                      log_headers])
-                    log_file.flush()
                         
                 time.sleep(10)
 
