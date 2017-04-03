383a1ffd7052873b1f86fa7394aec0aa11068c08
cassidylaidlaw <cassidy256@gmail.com>
2017-04-02 21:33:29 -0400

    Normalized language content in train_lda_mallet.py so as not to create imbalanced topics

--- scripts/train_lda_mallet.py
+++ scripts/train_lda_mallet.py
@@ -8,6 +8,38 @@ from os.path import sep
 from gensim.corpora import MmCorpus, Dictionary
 from gensim.models.wrappers import LdaMallet
 
+def reverse_tfidf(corpus):
+    for doc in corpus:
+        yield [(word, value * 200) for word, value in doc]
+        
+DICT_SIZE = 100000
+        
+def normalize_langs(corpus):
+    """
+    Normalizes the given corpus so that each language has the same number of
+    words for a given article.
+    """
+    
+    for doc in corpus:
+        doc.sort()
+        lang_docs = []
+        while len(doc) > 0:
+            lang_index = doc[0][0] // DICT_SIZE
+            lang_doc = []
+            while len(doc) > 0 and doc[0][0] // DICT_SIZE == lang_index:
+                lang_doc.append(doc.pop(0))
+            lang_docs.append(lang_doc)
+                
+        lang_lengths = [sum(freq for word, freq in lang_doc) for lang_doc
+                         in lang_docs]
+        max_length = max(lang_lengths)
+        normalized_doc = []
+        for lang_doc, lang_length in zip(lang_docs, lang_lengths):
+            for word, freq in lang_doc:
+                normalized_doc.append((word, freq * max_length /
+                                       lang_length))
+        yield normalized_doc
+
 if __name__ == '__main__':
     if len(sys.argv) not in range(4, 6):
         print('Usage: python3 train_lda.py corpus.bow.mm.bz2 dict.pickle model.lda.pickle (num_topics)')
@@ -29,6 +61,6 @@ if __name__ == '__main__':
         mm = MmCorpus(mm_fname)
         id2word = Dictionary.load(dict_fname)
         
-        lda_model = LdaMallet(mallet_path, corpus=corpus, id2word=id2word,
-                num_topics=num_topics)
+        lda_model = LdaMallet(mallet_path, corpus=normalize_langs(mm), 
+                id2word=id2word, num_topics=num_topics)
         lda_model.save(model_fname)
