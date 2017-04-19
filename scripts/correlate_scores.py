3e738ba1bb40b4512576fb28b44fa207caec08c1
cassidylaidlaw <cassidy256@gmail.com>
2017-04-19 17:05:53 -0400

    Added verbose option to correlate_scores.py

--- scripts/correlate_scores.py
+++ scripts/correlate_scores.py
@@ -4,14 +4,19 @@ import sys
 import csv
 import logging
 import numpy as np
+from scipy.stats.mstats import zscore
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python3 correlate_scores.py scores1.csv scores2.csv')
+    if len(sys.argv) not in range(3, 5):
+        print('Usage: python3 correlate_scores.py scores1.csv scores2.csv (verbose)')
         print('Reads the two files of chunk bias scores (as output by score_chunks.py or')
         print('mturk_results_to_scores.py) and logs the correlation between them to stdout.')
+        print('If the argument verbose is specified then it will also print a list of')
+        print('chunks in which the normalized bias (z-)scores deviated by the most to')
+        print('assist in determining why the algorithm performed well or badly.')
     else:
-        scores_fnames = sys.argv[1:]
+        scores_fnames = sys.argv[1:3]
+        verbose = len(sys.argv) >= 4 and sys.argv[3] == 'verbose'
         
         scores_dicts = []
         for scores_fname in scores_fnames:
@@ -33,3 +38,15 @@ if __name__ == '__main__':
             r = np.corrcoef(*scores_vectors)[0,1]
             logging.info('r = %f', r)
             logging.info('r^2 = %f', r ** 2)
+            
+            if verbose:
+                zscores_vectors = [zscore(scores) for scores in scores_vectors]
+                print('')
+                print('{:<70} {:>10} {:>10}'.format('Chunk ID', 'Score 1',
+                                                    'Score 2'))
+                print('-' * 92)
+                for index, score_diff in sorted(enumerate(np.abs(np.subtract(
+                        *zscores_vectors))), reverse=True, key=lambda x: x[1]):
+                    print('{:<70} {:>10.2f} {:>10.2f}'.format(
+                            ' '.join(chunk_ids[index]), zscores_vectors[0][index],
+                            zscores_vectors[1][index]))
